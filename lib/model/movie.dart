7350384ce09e2adfc576c2750e4b0dee813e3f25
Ferdian9991 <ferdisatria15@gmail.com>
2021-08-19 13:48:59 +0700

    remove null dependencies

--- lib/model/movie.dart
+++ lib/model/movie.dart
@@ -1,8 +1,3 @@
-import 'dart:convert';
-import 'dart:js';
-import 'package:flutter/cupertino.dart';
-import 'package:http/http.dart' as http;
-
 // class Search {
 //   String title;
 //   String link;
